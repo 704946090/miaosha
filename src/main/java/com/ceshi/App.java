eaa9fcc866eb29572f37f71dea54ecfe4076d992
liujinixn <704946090@qq.com>
2020-04-24 14:35:08 +0800

    秒杀项目

--- src/main/java/com/ceshi/App.java
+++ src/main/java/com/ceshi/App.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
- *秒杀项目a
+ *秒杀项目
  */
 @SpringBootApplication(scanBasePackages = {"com.ceshi"})
 @RestController
